7d49f809ebf3a4d7a3a0cf646d6c68dcc68235fe
ayodyabanuka <ayodyabanukafernando@gmail.com>
2024-09-08 01:25:18 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] });
 import Cookies from 'js-cookie';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
+import ProtectedRoute from '@/components/protected-route';
 
 export default function RootLayout({
   children
@@ -36,72 +37,76 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/logo1.png" type="image/x-icon" />
       </head>
       <body className={`${inter.className} min-h-screen w-full bg-gray-900`}>
-        <Provider store={store}>
-          <main
-            className={`${path === '/login' ? 'hidden' : 'block'} flex min-h-screen w-full`}
-          >
-            <div className="flex w-60 flex-col items-center gap-5 border-r border-gray-700 bg-gray-900 p-4">
-              <div className="flex flex-col items-center pb-5 text-white">
-                <Image
-                  src={'/logo1.png'}
-                  alt={'logo'}
-                  width={150}
-                  height={20}
-                  className=""
-                />
-              </div>
-              <div className="w-full px-2 py-2 font-light text-slate-500">
-                Main
-              </div>
-              <div className="flex w-full flex-col gap-4">
-                <Link
-                  href={'/'}
-                  className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/' ? 'bg-slate-800' : 'text-slate-400'}`}
-                >
-                  <IoMdAnalytics /> Overview
-                </Link>
-                <Link
-                  href={'/reservation'}
-                  className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/reservation' ? 'bg-slate-800' : 'text-slate-400'}`}
-                >
-                  <FaTicket /> Reservation
-                </Link>
-                <Link
-                  href={'/qr-scanner'}
-                  className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/qr-scanner' ? 'bg-slate-800' : 'text-slate-400'}`}
-                >
-                  <FaQrcode /> QR Scanner
-                </Link>
-              </div>
 
-              <div className="w-full px-2 py-2 font-light text-slate-500">
-                Settings
+        <Provider store={store}>
+          <ProtectedRoute>
+            <main
+              className={`${path === '/login' ? 'hidden' : 'block'} flex min-h-screen w-full`}
+            >
+              <div className="flex w-60 flex-col items-center gap-5 border-r border-gray-700 bg-gray-900 p-4">
+                <div className="flex flex-col items-center pb-5 text-white">
+                  <Image
+                    src={'/logo1.png'}
+                    alt={'logo'}
+                    width={150}
+                    height={20}
+                    className=""
+                  />
+                </div>
+                <div className="w-full px-2 py-2 font-light text-slate-500">
+                  Main
+                </div>
+                <div className="flex w-full flex-col gap-4">
+                  <Link
+                    href={'/'}
+                    className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/' ? 'bg-slate-800' : 'text-slate-400'}`}
+                  >
+                    <IoMdAnalytics /> Overview
+                  </Link>
+                  <Link
+                    href={'/reservation'}
+                    className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/reservation' ? 'bg-slate-800' : 'text-slate-400'}`}
+                  >
+                    <FaTicket /> Reservation
+                  </Link>
+                  <Link
+                    href={'/qr-scanner'}
+                    className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/qr-scanner' ? 'bg-slate-800' : 'text-slate-400'}`}
+                  >
+                    <FaQrcode /> QR Scanner
+                  </Link>
+                </div>
+
+                <div className="w-full px-2 py-2 font-light text-slate-500">
+                  Settings
+                </div>
+                <div className="flex w-full flex-col gap-4">
+                  <Link
+                    href={'/staff'}
+                    className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/staff' ? 'bg-slate-800' : 'text-slate-400'}`}
+                  >
+                    <FaPeopleRoof /> Staff
+                  </Link>
+                  <button
+                    onClick={handleSubmit}
+                    className="mt-10 flex w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-red-400 hover:bg-slate-800"
+                  >
+                    <IoLogOut /> Logout
+                  </button>
+                </div>
+                <div className="mt-auto flex w-full flex-col items-center gap-2 rounded-xl bg-slate-800 p-3">
+                  <div className="text-sm font-medium text-slate-400">{user}</div>
+                  <div className="text-xs font-light text-slate-500 ">Admin</div>
+                </div>
               </div>
-              <div className="flex w-full flex-col gap-4">
-                <Link
-                  href={'/staff'}
-                  className={`flex  w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-slate-400 hover:bg-slate-800 ${path === '/staff' ? 'bg-slate-800' : 'text-slate-400'}`}
-                >
-                  <FaPeopleRoof /> Staff
-                </Link>
-                <button
-                  onClick={handleSubmit}
-                  className="mt-10 flex w-full items-center gap-2 rounded-xl p-2 py-3 text-center text-red-400 hover:bg-slate-800"
-                >
-                  <IoLogOut /> Logout
-                </button>
-              </div>
-              <div className="mt-auto flex w-full flex-col items-center gap-2 rounded-xl bg-slate-800 p-3">
-                <div className="text-sm font-medium text-slate-400">{user}</div>
-                <div className="text-xs font-light text-slate-500 ">Admin</div>
-              </div>
+              <div className="w-full">{children}</div>
+            </main>
+            <div className={`${path === '/login' ? 'block' : 'hidden'}`}>
+              {children}
             </div>
-            <div className="w-full">{children}</div>
-          </main>
-          <div className={`${path === '/login' ? 'block' : 'hidden'}`}>
-            {children}
-          </div>
+          </ProtectedRoute>
         </Provider>
+
       </body>
     </html>
   );
--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,21 +1,28 @@
-import { ReactNode, useEffect } from 'react';
+"use client";
+import { ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import Login from '@/app/login/page';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-  const { user } = useSelector((state: RootState) => state.auth);
+  const { user } = useSelector((state: RootState) => state.auth) || {}; // Fallback to an empty object if state is null
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!user) {
       router.push('/login');
+    } else {
+      setIsLoading(false);
     }
   }, [user, router]);
 
-  // Optionally add a loading state here
+  if (isLoading) {
+    return <Login />;
+  }
 
-  return user ? children : null;
+  return user ? <>{children}</> : null;
 };
 
 export default ProtectedRoute;
